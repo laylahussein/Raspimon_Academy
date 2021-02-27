fcec78cff34620315b31b8099423d285259a35b9
laylahussein <laylahussein379@gmail.com>
2021-02-27 14:49:55 -0500

    begin work of sensehat

---
+++ raspimon_name.py
@@ -0,0 +1,40 @@
+from sense_hat import SenseHat   #import the SenseHat object from the sense_hat library
+from time import sleep
+
+#this is how you load the sense object so you can use it in this script.
+sense = SenseHat()
+
+#creating variables to store RGB colors
+p = (128, 0, 128) #pink
+w = (255, 255, 255) #white
+k = (0,0,0) #blank
+
+egg = [p,p,p,p,p,p,p,p,
+       p,p,p,w,w,p,p,p,
+       p,p,w,w,w,w,p,p,
+       p,p,w,w,w,w,p,p,
+       p,p,w,w,w,w,p,p,
+       p,p,w,w,w,w,p,p,
+       p,p,p,w,w,p,p,p,
+       p,p,p,p,p,p,p,p]
+       
+hatched = [k,k,k,k,k,k,k,k,
+           k,k,k,p,p,p,k,k,
+           k,k,p,p,p,p,p,k,
+           k,k,p,k,p,k,p,k,
+           k,k,p,w,p,w,p,k,
+           k,k,k,p,p,p,k,k,
+           k,k,k,p,p,p,k,k,
+           k,k,p,p,k,p,p,k ]
+
+#this is how you scroll a message
+sense.show_message("I'm still an egg!")
+
+#add more messages below. 
+sense.show_message("I'm ready to hatch soon!")
+sense.show_message("About to be hatched!", text_colour=[219,222,122], scroll_speed=0.4)
+
+#the sense object has functions we can use: set_pixels. The value in the parentheses should be the egg.
+sense.set_pixels(egg)
+
+
